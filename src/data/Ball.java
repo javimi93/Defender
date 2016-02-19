7275a65c23a3846ec95908b239a0d0ce8c5e812c
Javier <javie@DESKTOP-PSHBTSN>
2016-02-19 12:29:48 +0100

    Añadida imagen de sprite sin fondo y primera aproximacion de poner sprite nave comentada en la clase Ball

--- src/data/Ball.java
+++ src/data/Ball.java
@@ -3,7 +3,12 @@ package data;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
+
+import javax.imageio.ImageIO;
 
 public class Ball {
 	//Posiciones iniciales de la bola
@@ -14,7 +19,7 @@ public class Ball {
 	//yMovement es el desplazamiento de la bola sobre el eje y
 	int xMovement = 0;
 	int yMovement = 0;
-	//private BufferedImage image;
+	private BufferedImage image;
 	private static final int DIAMETER = 30;
 	private static final int ADD= 3;
 	private Game game;
@@ -26,15 +31,36 @@ public class Ball {
 
 	public Ball(Game game) {
 		this.game= game;
-		//TODO cargar imagen nave.
-		/*try
-        {
-            this.image = ImageIO.read(new File("datos/sprite.jpg"));
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }*/
+		/*BufferedImage bigImg;
+		try {
+			bigImg = ImageIO.read(new File("datos/sprite.png"));
+		
+		// The above line throws an checked IOException which must be caught.
+
+		final int width = 25;
+		final int height = 20;
+		final int rows = 2;
+		final int cols = 21;
+		BufferedImage[] sprites = new BufferedImage[rows * cols];
+
+		for (int i = 1; i < rows; i++)
+		{
+		    for (int j = 0; j < cols; j++)
+		    {
+		        sprites[(i * cols) + j] = bigImg.getSubimage(
+		            j * width,
+		            i * height,
+		            width,
+		            height
+		        );
+		    }
+		}
+		image=sprites[23];
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
 	}
 
 	void move() {
@@ -85,7 +111,7 @@ public class Ball {
 	}
 
 	public void paint(Graphics2D g) {
-		//g.drawImage(image, x, y, 20, 20, null);
+		//g.drawImage(image, xInit, yInit, 20, 20, null);
 		g.fillOval(xInit, yInit, DIAMETER, DIAMETER);
 		for(int i=0; i< shootsACTIVOS;i++){
 			paint=shoots.get(i).paint(g,lastXa);
